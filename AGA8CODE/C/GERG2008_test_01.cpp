fe6db1e09ffcad1b22c4dd6d25165465a3369b16
Roy Vegard Ovesen <roy.vegard.ovesen@gmail.com>
2019-11-08 17:11:50 +0100

    Gerg and gross test (#11)
    * Add tests for GERG2008 and Gross
    
    * Proper compare in Gross test
    
    And whitespace.
    
    * Change some test tolerances
    
    * Update dPdD2_reference value, fixes #8
    
    * Fix variable name typo
    
    * Tighten the test tolerances
    
    * Tighten tolerances and fix variable name typo
    
    * Use return value macros and std::abs
    
    * Include cstdlib to get the return value macros

--- AGA8CODE/C/GERG2008_test_01.cpp
+++ AGA8CODE/C/GERG2008_test_01.cpp
@@ -2,50 +2,49 @@
 #include <math.h>
 #include <cstdlib>
 #include <cstdio>
-#include "Detail.h"
+#include "GERG2008.h"
 
 int main()
 {
-    SetupDetail();
+    SetupGERG();
     double _x[] = {0.77824, 0.02, 0.06, 0.08, 0.03, 0.0015, 0.003, 0.0005,
         0.00165, 0.00215, 0.00088, 0.00024, 0.00015, 0.00009, 0.004, 0.005,
         0.002, 0.0001, 0.0025, 0.007, 0.001};
-    const int NcDetail = 21;
-    std::vector<double> x(_x, _x+NcDetail), xGrs(4,0);
+    const int NcGERG = 21;
+    std::vector<double> x(_x, _x+NcGERG), xGrs(4,0);
     x.insert(x.begin(), 0.0);
     double mm = 0;
-    MolarMassDetail(x, mm);
+    MolarMassGERG(x, mm);
 
     int ierr = 0;
     std::string herr;
-    double T = 400, P = 50000, D = 1e10;
-    DensityDetail(T, P, x, D, ierr, herr);
-
-    double Z, dPdD, d2PdD2, d2PdTD, dPdT, U, H, S, Cv, Cp, W, G, JT, Kappa;
-    PropertiesDetail(T, D, x, P, Z, dPdD, d2PdD2, d2PdTD, dPdT, U, H, S,
-        Cv, Cp, W, G, JT, Kappa);
+    double T = 400, P = 50000, D = 6.36570, Z = 0;
+    double dPdD, d2PdD2, d2PdTD, dPdT, U, H, S, Cv, Cp, W, G, JT, Kappa, A;
+    DensityGERG(0, T, P, x, D, ierr, herr);
+    PropertiesGERG(T, D, x, P, Z, dPdD, d2PdD2, d2PdTD, dPdT, U, H, S,
+        Cv, Cp, W, G, JT, Kappa, A);
 
     int return_value = EXIT_SUCCESS;
-    double mm_reference    = 20.54333051;
-    double D_reference     = 12.80792403648801;
+    double mm_reference    = 20.5427445016;
+    double D_reference     = 12.79828626082062;
     double P_reference     = 50000.0;
-    double Z_reference     = 1.173801364147326;
-    double dPdD_reference  = 6971.387690924090;
-    double d2PdD2_reference = 1118.803636639520;
-    double dPdT_reference  = 235.6641493068212;
-    double U_reference     = -2739.134175817231;
-    double H_reference     = 1164.699096269404;
-    double s_reference     = -38.54882684677111;
-    double Cv_reference    = 39.12076154430332;
-    double Cp_reference    = 58.54617672380667;
-    double W_reference     = 712.6393684057903;
-    double G_reference     = 16584.22983497785;
-    double JT_reference    = 7.432969304794577E-05;
-    double Kappa_reference = 2.672509225184606;
+    double Z_reference     = 1.174690666383717;
+    double dPdD_reference  = 7000.694030193327;
+    double d2PdD2_reference = 1129.526655214841;
+    double dPdT_reference  = 235.9832292593096;
+    double U_reference     = -2746.492901212530;
+    double H_reference     = 1160.280160510973;
+    double s_reference     = -38.57590392409089;
+    double Cv_reference    = 39.02948218156372;
+    double Cp_reference    = 58.45522051000366;
+    double W_reference     = 714.4248840596024;
+    double G_reference     = 16590.64173014733;
+    double JT_reference    = 7.155629581480913E-05;
+    double Kappa_reference = 2.683820255058032;
 
     printf("Inputs-----\n");
-    printf("Temperature [K]:                    400.0000000000000 != %0.16g\n",T);
-    printf("Pressure [kPa]:                     50000.00000000000 != %0.16g\n",P);
+    printf("Temperature [K]:                    400.0000000000000 != %0.16g\n", T);
+    printf("Pressure [kPa]:                     50000.00000000000 != %0.16g\n", P);
     printf("Outputs-----\n");
     if (std::abs(mm_reference - mm) > 1.0e-8)
     {
@@ -63,7 +62,7 @@ int main()
     {
         printf("Pressure [kPa]:                     %0.16g != %0.16g\n",
             P_reference, P);
-        return_value = 1;
+        return_value = EXIT_FAILURE;
     }
     if (std::abs(Z_reference - Z) > 1.0e-8)
     {
