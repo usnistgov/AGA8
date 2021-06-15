3ee0f53244e6586826672aab95b1734c4bf2ce0a
Joaquín Torrens <71098233+TorrensJoaquin@users.noreply.github.com>
2021-06-14 23:32:25 -0300

    Update 01_Index.js

--- AGA8CODE/JS/Example Of Implementation/01_Index.js
+++ AGA8CODE/JS/Example Of Implementation/01_Index.js
@@ -135,6 +135,7 @@ function CreateInputsIn00(){
     aux=aux+30;
 }
 function mousePressed(){
+    if (event.type != 'mousedown') return true;   //Avoid double click problem on mobile devices.
     webButtons[0].checkIfThisClickIsForMe();
     webButtons[1].checkIfThisClickIsForMe();
     webButtons[2].checkIfThisClickIsForMe();
